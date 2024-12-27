0cd67372f2131f8fadd07a18aa607dab4f458bae
AmVoidGuy <132288247+AmVoidGuy@users.noreply.github.com>
2024-12-27 15:54:26 -0500

    Adding profit to sim all

--- dist/bundle.js
+++ dist/bundle.js
@@ -2380,22 +2380,192 @@ function manipulateSimResultsDataForDisplay(simResults){
                 }
                 experiencePerHour[skill] = experiencePerHourValue;
             });
-            showKills(simResult, playerToDisplay);
-            showConsumablesUsed(simResult, playerToDisplay);
+            getDropProfit(simResult, playerToDisplay);
+            let profit = simResult["profit"];
+            let noRngProfit = simResult["noRngProfit"];
 
             let displaySimRow = {"ZoneName": zoneName, "Player": playerToDisplay, "Encounters": encountersPerHour, "Deaths": deathsPerHour,
                                 "TotalExperience": totalExperiencePerHour, "Stamina": experiencePerHour["Stamina"], 
                                 "Intelligence": experiencePerHour["Intelligence"], "Attack": experiencePerHour["Attack"],
                                 "Magic": experiencePerHour["Magic"], "Ranged": experiencePerHour["Ranged"],
-                                "Power": experiencePerHour["Power"], "Defense": experiencePerHour["Defense"], 
-                                "noRngRevenue": window.noRngRevenue.toLocaleString('en-US', {minimumFractionDigits: 2,maximumFractionDigits: 2}),
-                                "expenses": window.expenses.toLocaleString('en-US', {minimumFractionDigits: 2,maximumFractionDigits: 2}),
-                                "noRngProfit": (window.noRngRevenue - window.expenses).toLocaleString('en-US', {minimumFractionDigits: 2,maximumFractionDigits: 2})
-                            };
+                                "Power": experiencePerHour["Power"], "Defense": experiencePerHour["Defense"], "Profit": profit, "NoRNGProfit": noRngProfit};
             displaySimResults.push(displaySimRow);
         }
     }
     return displaySimResults;
+}
+
+function getDropProfit(simResult, playerToDisplay) {
+    let dropRateMultiplier = simResult.dropRateMultiplier;
+    let rareFindMultiplier = simResult.rareFindMultiplier;
+    let numberOfPlayers = simResult.numberOfPlayers;
+       let monsters = Object.keys(simResult.deaths)
+        .filter(enemy => enemy !== "player1" && enemy !== "player2" && enemy !== "player3" && enemy !== "player4" && enemy !== "player5")
+        .sort();
+
+    const totalDropMap = new Map();
+    const noRngTotalDropMap = new Map();
+    for (const monster of monsters) {
+        const dropMap = new Map();
+        const rareDropMap = new Map();
+        if(_combatsimulator_data_combatMonsterDetailMap_json__WEBPACK_IMPORTED_MODULE_13__[monster].dropTable) {
+            for (const drop of _combatsimulator_data_combatMonsterDetailMap_json__WEBPACK_IMPORTED_MODULE_13__[monster].dropTable) {
+                if (drop.minEliteTier > simResult.eliteTier) {
+                    continue;
+                }
+                dropMap.set(_combatsimulator_data_itemDetailMap_json__WEBPACK_IMPORTED_MODULE_3__[drop.itemHrid]['name'], { "dropRate": Math.min(1, drop.dropRate * dropRateMultiplier), "number": 0, "dropMin": drop.minCount, "dropMax": drop.maxCount, "noRngDropAmount": 0 });
+            }
+            if(_combatsimulator_data_combatMonsterDetailMap_json__WEBPACK_IMPORTED_MODULE_13__[monster].rareDropTable)
+            for (const drop of _combatsimulator_data_combatMonsterDetailMap_json__WEBPACK_IMPORTED_MODULE_13__[monster].rareDropTable) {
+                if (drop.minEliteTier > simResult.eliteTier) {
+                    continue;
+                }
+                rareDropMap.set(_combatsimulator_data_itemDetailMap_json__WEBPACK_IMPORTED_MODULE_3__[drop.itemHrid]['name'], { "dropRate": drop.dropRate * rareFindMultiplier, "number": 0, "dropMin": drop.minCount, "dropMax": drop.maxCount, "noRngDropAmount": 0 });
+            }
+
+            for (let dropObject of dropMap.values()) {
+                dropObject.noRngDropAmount += simResult.deaths[monster] * dropObject.dropRate * ((dropObject.dropMax + dropObject.dropMin) / 2) / numberOfPlayers;
+            }
+            for (let dropObject of rareDropMap.values()) {
+                dropObject.noRngDropAmount += simResult.deaths[monster] * dropObject.dropRate * ((dropObject.dropMax + dropObject.dropMin) / 2) / numberOfPlayers;
+            }
+
+            for (let i = 0; i < simResult.deaths[monster]; i++) {
+                for (let dropObject of dropMap.values()) {
+                    let chance = Math.random();
+                    if (chance <= dropObject.dropRate) {
+                        let amount = Math.floor(Math.random() * (dropObject.dropMax - dropObject.dropMin + 1) + dropObject.dropMin)
+                        dropObject.number = dropObject.number + amount;
+                    }
+                }
+                for (let dropObject of rareDropMap.values()) {
+                    let chance = Math.random();
+                    if (chance <= dropObject.dropRate) {
+                        let amount = Math.floor(Math.random() * (dropObject.dropMax - dropObject.dropMin + 1) + dropObject.dropMin)
+                        dropObject.number = dropObject.number + amount;
+                    }
+                }
+            }
+            for (let [name, dropObject] of dropMap.entries()) {
+                if (totalDropMap.has(name)) {
+                    totalDropMap.set(name, Math.round((totalDropMap.get(name) + dropObject.number) / numberOfPlayers));
+                } else {
+                    totalDropMap.set(name, Math.round(dropObject.number / numberOfPlayers));
+                }
+                if (noRngTotalDropMap.has(name)) {
+                    noRngTotalDropMap.set(name, noRngTotalDropMap.get(name) + dropObject.noRngDropAmount);
+                } else {
+                    noRngTotalDropMap.set(name, dropObject.noRngDropAmount);
+                }
+            }
+            for (let [name, dropObject] of rareDropMap.entries()) {
+                if (totalDropMap.has(name)) {
+                    totalDropMap.set(name, totalDropMap.get(name) + dropObject.number);
+                } else {
+                    totalDropMap.set(name, dropObject.number);
+                }
+                if (noRngTotalDropMap.has(name)) {
+                    noRngTotalDropMap.set(name, noRngTotalDropMap.get(name) + dropObject.noRngDropAmount);
+                } else {
+                    noRngTotalDropMap.set(name, dropObject.noRngDropAmount);
+                }
+            }
+        }
+    }
+    let total = 0;
+    for (let [name, dropAmount] of totalDropMap.entries()) {
+        let price = -1;
+        let revenueSetting = document.getElementById('selectPrices_drops').value;
+        if (window.prices) {
+            let item = window.prices[name];
+            if (item) {
+                if (revenueSetting == 'bid') {
+                    if (item['bid'] !== -1) {
+                        price = item['bid'];
+                    } else if (item['ask'] !== -1) {
+                        price = item['ask'];
+                    }
+                } else if (revenueSetting == 'ask') {
+                    if (item['ask'] !== -1) {
+                        price = item['ask'];
+                    } else if (item['bid'] !== -1) {
+                        price = item['bid'];
+                    }
+                }
+                if (price == -1) {
+                    price = item['vendor'];
+                }
+            }
+        }
+        total += price * dropAmount;
+    }
+
+    let noRngTotal = 0;
+    for (let [name, dropAmount] of noRngTotalDropMap.entries()) {
+        let price = -1;
+        let revenueSetting = document.getElementById('selectPrices_drops').value;
+        if (window.prices) {
+            let item = window.prices[name];
+            if (item) {
+                if (revenueSetting == 'bid') {
+                    if (item['bid'] !== -1) {
+                        price = item['bid'];
+                    } else if (item['ask'] !== -1) {
+                        price = item['ask'];
+                    }
+                } else if (revenueSetting == 'ask') {
+                    if (item['ask'] !== -1) {
+                        price = item['ask'];
+                    } else if (item['bid'] !== -1) {
+                        price = item['bid'];
+                    }
+                }
+                if (price == -1) {
+                    price = item['vendor'];
+                }
+            }
+        }
+        noRngTotal += price * dropAmount;
+    }
+
+    let consumablesUsed = simResult.consumablesUsed?.[playerToDisplay];
+
+    if (consumablesUsed) {
+      consumablesUsed = Object.entries(consumablesUsed).sort((a, b) => b[1] - a[1]);
+    } else {
+      consumablesUsed = []; 
+    }
+
+    let expenses = 0;
+    for (const [consumable, amount] of consumablesUsed) {
+        let price = -1;
+        let expensesSetting = document.getElementById('selectPrices_consumables').value;
+        if (window.prices) {
+            let item = window.prices[_combatsimulator_data_itemDetailMap_json__WEBPACK_IMPORTED_MODULE_3__[consumable].name];
+            if (item) {
+                if (expensesSetting == 'bid') {
+                    if (item['bid'] !== -1) {
+                        price = item['bid'];
+                    } else if (item['ask'] !== -1) {
+                        price = item['ask'];
+                    }
+                } else if (expensesSetting == 'ask') {
+                    if (item['ask'] !== -1) {
+                        price = item['ask'];
+                    } else if (item['bid'] !== -1) {
+                        price = item['bid'];
+                    }
+                }
+                if (price == -1) {
+                    price = item['vendor'];
+                }
+            }
+        }
+        expenses += price * amount;
+    }
+
+    simResult["profit"] = (total - expenses).toLocaleString();
+    simResult["noRngProfit"] = (noRngTotal - expenses).toLocaleString();
 }
 
 function updateAllSimsModal(data) {
@@ -2412,32 +2582,6 @@ function updateAllSimsModal(data) {
         
         tableBody.appendChild(row);
     });
-
-    const table = document.getElementById('allZonesData');
-    const rows = table.getElementsByTagName('tr');
-    const numCols = rows[0].cells.length;
-  
-    // 遍历每一列
-    for (let col = 4; col < numCols; col++) {
-      let max = -Infinity;
-      let maxCell = null;
-  
-      // 找到最大值及其单元格
-      for (let row = 1; row < rows.length; row++) {
-        const cell = rows[row].cells[col];
-        const value = parseFloat(cell.textContent);
-        if (value > max) {
-          max = value;
-          maxCell = cell;
-        }
-      }
-  
-      // 将最大值单元格的背景色设置为绿色
-      if (maxCell && max != 0) {
-        maxCell.style.backgroundColor = 'green';
-        maxCell.style.color = 'white'; // 设置文字颜色为白色以提高可读性
-      }
-    }
 }
 
 let currentSortColumn = null;
@@ -2535,8 +2679,7 @@ function showKills(simResult, playerToDisplay) {
         let totalDungeonsRow = createRow(["col-md-6", "col-md-6 text-end"], ["Total Dungeons", simResult.dungeonsCompleted]);
         newChildren.push(totalDungeonsRow);
         encountersPerHour = (simResult.dungeonsCompleted / hoursSimulated).toFixed(1);
-        let averageTime = (hoursSimulated * 60 / simResult.dungeonsCompleted).toFixed(1);
-        encountersRow = createRow(["col-md-6", "col-md-6 text-end"], ["Average Time", averageTime]);
+        encountersRow = createRow(["col-md-6", "col-md-6 text-end"], ["Dungeons / hr", encountersPerHour]);
     } else {
         encountersPerHour = (simResult.encounters / hoursSimulated).toFixed(1);
         encountersRow = createRow(["col-md-6", "col-md-6 text-end"], ["Encounters", encountersPerHour]);
@@ -3369,7 +3512,7 @@ function startSimulation(selectedPlayers) {
         worker.postMessage(workerMessage);
     } else {
         let zoneHrids = Object.values(_combatsimulator_data_actionDetailMap_json__WEBPACK_IMPORTED_MODULE_12__)
-        .filter((action) => action.type == "/action_types/combat" && action.category != "/action_categories/combat/dungeons" && action.combatZoneInfo.fightInfo.battlesPerBoss === 10)
+        .filter((action) => action.type == "/action_types/combat" && action.category != "/action_categories/combat/dungeons")
         .sort((a, b) => a.sortIndex - b.sortIndex)
         .map(action => action.hrid);
         let workerMessage = {
@@ -4041,11 +4184,7 @@ window.prices;
 
 async function fetchPrices() {
     try {
-        const response = await fetch('https://ghproxy.net/https://raw.githubusercontent.com/holychikenz/MWIApi/refs/heads/main/milkyapi.json'
-            , {
-                mode: 'cors'
-              }
-        );
+        const response = await fetch('https://raw.githubusercontent.com/holychikenz/MWIApi/main/milkyapi.json');
         if (!response.ok) {
             throw new Error('Error fetching prices');
         }
